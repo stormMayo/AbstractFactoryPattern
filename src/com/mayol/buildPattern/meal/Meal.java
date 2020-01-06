017b19831b89a7ee1005023585c0e3b749e11381
stormMayo <stormmayol@gmail.com>
2020-01-06 21:06:00 +0800

    AbstractFactoryPattern

---
+++ src/com/mayol/buildPattern/meal/Meal.java
@@ -0,0 +1,12 @@
+package com.mayol.buildPattern.meal;
+
+import com.mayol.buildPattern.food.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Meal {
+    private List<Item> items = new ArrayList<>();
+    
+
+}
