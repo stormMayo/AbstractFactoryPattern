7bff304e38efd07670733ba97cb9d2372f1deb9a
stormMayo <stormmayol@gmail.com>
2020-01-15 16:27:09 +0800

    CompositePattern

---
+++ src/com/mayol/compositePattern/entity/Employee.java
@@ -0,0 +1,41 @@
+package com.mayol.compositePattern.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
